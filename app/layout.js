55823f953deb0c8b60f1bd6a7c2e30bfbce72820
chrislam365 <cllam@wpi.edu>
2025-06-19 20:59:15 -0400

    file directories and base components for page layout

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import {Theme} from "@radix-ui/themes";
+import {Navbar} from "@/components/navbar";
+import Contact from "@/components/contact";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -24,7 +26,9 @@ export default function RootLayout({children}) {
             className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
         <Theme>
+            <Navbar/>
             {children}
+            <Contact/>
         </Theme>
         </body>
         </html>
